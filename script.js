db94ad0e473929388439aec1f70839a3a0f00d47
Samuel-Muli <salmmuelmully@gmail.com>
2025-03-12 09:05:04 +0300

    This will be great.

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new typed('.text', {
+    strings: ["Mechanical Engineer", "Coder", "Web Developer", "Designer", "Freelancer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    backDelay: 1000
+});
