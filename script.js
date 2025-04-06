69345bad48493be2549c32646ccab773c5242665
Samuel-Muli <salmmuelmully@gmail.com>
2025-04-06 09:07:11 +0300

    just here and there shifts not great still don't know why i keep coming back to this

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-var typed = new typed('.text', {
+var typed = new Typed('.text-typed', {
     strings: ["Mechanical Engineer", "Coder", "Web Developer", "Designer", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop:true
+    loop: true
 });
